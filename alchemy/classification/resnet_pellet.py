7d372bf233cecb4e0a90792bba1207ec43c3806c
syz247179876 <247179876@qq.com>
2023-10-14 23:37:24 +0800

    add pellet of ResNet, ReXtNet

---
+++ alchemy/classification/resnet_pellet.py
@@ -0,0 +1,43 @@
+import sys
+if r'D:\projects\cv-deep-learning' not in sys.path:
+    sys.path.append(r'D:\projects\cv-deep-learning')
+from network import resnet50, resnet101, resnet152
+from alchemy.utils import Args, ClassifyLoss, basic_run
+
+
+class ResNetArgs(Args):
+    """
+    ResNetArgs Args
+    """
+
+    def __init__(self):
+        """
+        if add new args, plz callback self.parser.add_argument before super(ResNetArgs, self).__init__()
+        """
+        super(ResNetArgs, self).__init__()
+        self.set_train_args()
+
+
+class ResNetLoss(ClassifyLoss):
+
+    def __init__(self):
+        super(ResNetLoss, self).__init__()
+
+
+def resnet_run(model_name: str, args: ResNetArgs, loss_obj: ResNetLoss):
+    """
+    standard resnet family training
+    """
+    model = globals().get(model_name, None)(num_classes=5, classifier=True)
+    assert model is not None, f'{model_name} not defined'
+    basic_run(model, model_name, args, loss_obj)
+
+
+if __name__ == '__main__':
+    """
+    note: 
+    Starting from the command line requires adding a model_name parameter
+    """
+    args_ = ResNetArgs()
+    loss_obj_ = ResNetLoss()
+    resnet_run(args_.opts.model_name, args_, loss_obj_)
---
+++ alchemy/classification/resnext_pellet.py
@@ -0,0 +1,43 @@
+import sys
+if r'D:\projects\cv-deep-learning' not in sys.path:
+    sys.path.append(r'D:\projects\cv-deep-learning')
+from network import ResNeXt50_32x4d, ResNeXt101_32x4d, ResNeXt152_32x4d
+from alchemy.utils import Args, ClassifyLoss, basic_run
+
+
+class ResNeXtArgs(Args):
+    """
+    ResNeXtArgs Args
+    """
+
+    def __init__(self):
+        """
+        if add new args, plz callback self.parser.add_argument before super(ResNeXtArgs, self).__init__()
+        """
+        super(ResNeXtArgs, self).__init__()
+        self.set_train_args()
+
+
+class ResNeXtLoss(ClassifyLoss):
+
+    def __init__(self):
+        super(ResNeXtLoss, self).__init__()
+
+
+def resnext_run(model_name: str, args: ResNeXtArgs, loss_obj: ResNeXtLoss):
+    """
+    ResNeXt family training
+    """
+    model = globals().get(model_name, None)(num_classes=5, classifier=True)
+    assert model is not None, f'{model_name} not defined'
+    basic_run(model, model_name, args, loss_obj)
+
+
+if __name__ == '__main__':
+    """
+    note: 
+    Starting from the command line requires adding a model_name parameter
+    """
+    args_ = ResNeXtArgs()
+    loss_obj_ = ResNeXtLoss()
+    resnext_run(args_.opts.model_name, args_, loss_obj_)
